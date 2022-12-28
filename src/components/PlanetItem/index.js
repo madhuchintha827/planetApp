c0567f56a7f44af32e95bc8582c259fd58be9f25
madhuchintha827 <chinthamadhu827@gmail.com>
2022-12-28 03:54:50 +0000

    solution

---
+++ src/components/PlanetItem/index.js
@@ -0,0 +1,17 @@
+// Write your code here
+import './index.css'
+
+const PlanetItem = props => {
+  const {planetDetails} = props
+  const {name, imageUrl, description} = planetDetails
+
+  return (
+    <div className="planet-container">
+      <img src={imageUrl} alt={`planet ${name}`} className="img" />
+      <h1 className="planet-name">{name}</h1>
+      <p className="planet-para">{description}</p>
+    </div>
+  )
+}
+
+export default PlanetItem
---
+++ src/components/PlanetsSlider/index.js
@@ -0,0 +1,28 @@
+// Write your code here
+import Slider from 'react-slick'
+
+import PlanetItem from '../PlanetItem'
+
+import './index.css'
+
+const PlanetsSlider = props => {
+  const {planetsList} = props
+  const settings = {
+    dots: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  }
+  return (
+    <div testId="planets" className="bg-container">
+      <h1 className="title">PLANETS</h1>
+      <Slider {...settings}>
+        {planetsList.map(eachPlanet => (
+          <PlanetItem planetDetails={eachPlanet} key={eachPlanet.id} />
+        ))}
+      </Slider>
+    </div>
+  )
+}
+
+export default PlanetsSlider
